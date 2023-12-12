21622ea79851fb98eff664098d0f1713e221765c
AmrTamer23 <amr12300062@cs.modern-academy.edu.eg>
2023-12-12 03:56:21 +0200

    Hotfix to deploy #2

--- src/app/lib/api/main-category.ts
+++ src/app/lib/api/main-category.ts
@@ -2,10 +2,10 @@ import { instance } from "../instance";
 
 export const fetchMainCategories = async (
   type?: PathOptions
-): Promise<MainCategories> => {
+): Promise<MainCategory> => {
   const res = await instance.get(`/main-categories?populate=*`);
 
-  let data: MainCategories = res.data;
+  let data: MainCategory = res.data;
 
   return data;
 };
